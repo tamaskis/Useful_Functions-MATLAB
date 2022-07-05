7c1b57b3cab2c311b12ac26757167a995d5b8852
tamaskis <tamas.a.kis@outlook.com>
2022-07-05 18:26:28 -0400

    updated all functions/examples

--- examples/EXAMPLES_waitbar.m
+++ examples/EXAMPLES_waitbar.m
@@ -4,7 +4,7 @@
 % functions.
 %
 % Copyright © 2022 Tamas Kis
-% Last Update: 2022-06-04
+% Last Update: 2022-07-05
 % Website: https://tamaskis.github.io
 % Contact: tamas.a.kis@outlook.com
 
--- functions/interpshape.m
+++ functions/interpshape.m
@@ -58,27 +58,10 @@ function [x_new,y_new] = interpshape(x,y,n)
     
     % calculates interpolated points
     for i = 1:(N-1)
-        
-        % first point forming line
-        x1 = x(i);
-        y1 = y(i);
-        
-        % second point forming line
-        x2 = x(i+1);
-        y2 = y(i+1);
-        
-        % angle between two points
-        theta = atan2(y2-y1,x2-x1);
-        
-        % distance between two points
-        l = sqrt((x2-x1)^2+(y2-y1)^2);
-        
-        % interpolated points
         for j = 1:n
-            x_interp(n*(i-1)+j) = x1+(j/(n+1))*l*cos(theta);
-            y_interp(n*(i-1)+j) = y1+(j/(n+1))*l*sin(theta);
+            x_interp(n*(i-1)+j) = x(i)+(j/(n+1))*(x(i+1)-x(i));
+            y_interp(n*(i-1)+j) = y(i)+(j/(n+1))*(y(i+1)-y(i));
         end
-        
     end
     
     % "fuses" arrays (places interpolated points at correct indices)
